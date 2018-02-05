c514728c84217acd9afc57a52799a19ab45ecc14
Senne Deproost <sennedeproost@hotmail.com>
2018-02-05 17:46:24 +0100

    Changed OpenMDAO folder

---
+++ WISDEM/OpenMDAO/openmdao-0.13.0/Scripts/mkpseudo-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Senne Deproost\OneDrive - Vrije Universiteit Brussel\2017-2018\BP\Bachelorproef\MICO\WISDEM\OpenMDAO\openmdao-0.13.0\Scripts\python2.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'openmdao.util==0.13.0','console_scripts','mkpseudo'
+__requires__ = 'openmdao.util==0.13.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('openmdao.util==0.13.0', 'console_scripts', 'mkpseudo')()
+    )
---
+++ WISDEM/OpenMDAO/openmdao-0.13.0/Scripts/rst2latex.py
@@ -0,0 +1,4 @@
+#!"C:\Users\Senne Deproost\OneDrive - Vrije Universiteit Brussel\2017-2018\BP\Bachelorproef\MICO\WISDEM\OpenMDAO\openmdao-0.13.0\Scripts\python2.exe"
+# EASY-INSTALL-SCRIPT: 'docutils==0.10','rst2latex.py'
+__requires__ = 'docutils==0.10'
+__import__('pkg_resources').run_script('docutils==0.10', 'rst2latex.py')
