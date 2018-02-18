c77f9706324432da067fb80987fc3ddd713e47dd
Senne Deproost <sennedeproost@hotmail.com>
2018-02-18 16:58:59 +0100

    Added shell script

--- infrastructure.py
+++ infrastructure.py
@@ -1,7 +1,10 @@
-import tinydb
+from tinydb import *
 
-db = TinyDB("testpark_config.py")
+def loadModel(model):
+    exec("import %s" % ("models." + model.replace(" ", "_") + ".py") + " *")
 
 def loadInfraConfig(path):
     db = TinyDB(path)
     return db
+
+loadInfraConfig("testpark_config.json")
---
+++ models/NREL_5MW.py
@@ -0,0 +1,15 @@
+
+# Extern parameter is a object used to determine dependencies in the
+# database. Queries are done in a TinyDB database.
+def searchDependencies(database, extern):
+    dependencyList = []
+    windDirection = extern.windDirection
+
+    
+
+
+
+    return dependencyList
+
+def dependency(a, b):
+    return "lala"
