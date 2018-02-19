81aa1c63da05580d95e64a25370e821d0a3571aa
Senne Deproost <sennedeproost@hotmail.com>
2018-02-19 01:15:21 +0100

    Added OpenMDAO environement for Windows

---
+++ WISDEM/OpenMDAO/openmdao-0.13.0/Scripts/sphinx-quickstart-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Senne Deproost\OneDrive - Vrije Universiteit Brussel\2017-2018\BP\Bachelorproef\MICO\WISDEM\OpenMDAO\openmdao-0.13.0\Scripts\python2.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'sphinx==1.2.2','console_scripts','sphinx-quickstart'
+__requires__ = 'sphinx==1.2.2'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('sphinx==1.2.2', 'console_scripts', 'sphinx-quickstart')()
+    )
