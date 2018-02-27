7e9054783f7ec768127c2b69800c84e3cd063dab
Senne Deproost <sennedeproost@hotmail.com>
2018-02-27 18:24:48 +0100

    Basic functionality for infrastructure to database

--- infrastructure.py
+++ infrastructure.py
@@ -1,15 +1,39 @@
-from tinydb import *
+from tinydb import TinyDB, Query
 from matplotlib import pyplot as plt
 import globals as g
+import json as j
+import os.path
 
 def loadInfrastructure(config):
-    g.printStat("Loaded " + config + " into infrastructure database.")
+    extension = ".json"
+    if os.path.isfile(config) and "_TinyDB" in config:
+        db_name = file[:-5] + "_TinyDB"
+        db = TinyDB(db_name + extension)
+        g.printStat("Loaded " + config + " into infrastructure database.")
+        return db
+    else:
+        db = convertJsonToDB(config)
+        if db:
+            g.printStat("Loaded " + config + " into infrastructure database.")
+            return db
+        else: g.printStat("ERROR: " + file + "IS NOT A VALID EXTENSION TO CONVERT TO DATABASE.")
 
-    db = TinyDB("config")
 
-    infra = ["number", "dimensions", db]
 
-    return infra
+def convertJsonToDB(file):
+    extension = ".json"
+    if validFormat(file, extension):
+        data = j.load(open(file))
+        db_name = file[:-5] + "_TinyDB"
+        db = TinyDB(db_name + extension)
+        for obj in data:
+            db.insert(obj)
+        return db
+    else:
+        g.printStat("ERROR: " + file + "IS NOT A VALID EXTENSION TO CONVERT TO DATABASE.")
+        return False
 
-def mapInfrastructre(infra):
-    return "haha"
+
+
+def validFormat(file, extension):
+    return extension in file
