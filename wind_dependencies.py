b6a5ffe0a10e9357c72f362918ddccb19b8b81c5
Senne Deproost <sennedeproost@hotmail.com>
2018-03-14 23:26:35 +0100

    Goodbye matplotlib, hello sympy!

--- wind_dependencies.py
+++ wind_dependencies.py
@@ -1,10 +1,15 @@
 import math as m
 import matplotlib.pyplot as plt
+from sympy import *
+from sympy.geometry import *
+import numpy as np
 import matplotlib.patches as patches
 
 # Dependency lies within a wake zone that is represented by a rectangle with as width the diameter of the blades of the
 # and as length infinity.
 
+plt.xlim(-200, 200)
+plt.ylim(-200, 200)
 
 class Point:
     def __init__(self,x_init,y_init):
@@ -26,20 +31,12 @@ def inWake(a, b, a_angle, wind):
 
     a_angle = m.radians(a_angle)
 
-    print a_angle
-
     a_slope = m.tan(a_angle)
 
-    print a_slope
-
     corrected_a_angle = a_angle + m.pi/2
-    x_corr = m.sin(corrected_a_angle)*radius
-    y_corr = m.cos(corrected_a_angle)*radius
 
-
-    print a_x + m.fabs(m.sin(corrected_a_angle)*radius)
-
-
+    x_corr = m.cos(corrected_a_angle)*radius
+    y_corr = m.sin(corrected_a_angle)*radius
 
     blade_point_a1 = Point(
         a_x + x_corr,
@@ -51,10 +48,44 @@ def inWake(a, b, a_angle, wind):
         a_y - y_corr
     )
 
+    wind_angle = m.radians(wind_angle) + m.pi
+    print(wind_angle)
+    zone_length = 200
 
+    x_corr = m.cos(wind_angle)*zone_length
+    y_corr = m.sin(wind_angle)*zone_length
+
+    zone_end1 = Point(
+        blade_point_a1.x + x_corr,
+        blade_point_a1.y + y_corr
+    )
+
+    zone_end2 = Point(
+        blade_point_a2.x + x_corr,
+        blade_point_a2.y + y_corr
+    )
+
+
+    plt.plot([blade_point_a1.x, blade_point_a2.x], [blade_point_a1.y, blade_point_a2.y], "black")
     plt.plot(a_x, a_y, "bo")
     plt.plot(blade_point_a1.x, blade_point_a1.y, "r+")
     plt.plot(blade_point_a2.x, blade_point_a2.y, "r+")
+
+    b_circle = plt.Circle((b["location"]["x"], b["location"]["y"]), radius)
+
+
+
+
+    plt.plot(zone_end1.x, zone_end1.y, "g+")
+    plt.plot(zone_end2.x, zone_end2.y, "g+")
+
+    plt.plot([blade_point_a1.x, zone_end1.x], [blade_point_a1.y, zone_end1.y], "orange")
+    plt.plot([blade_point_a2.x, zone_end2.x], [blade_point_a2.y, zone_end2.y], "orange")
+
+    plt.xlim(-200, 200)
+    plt.ylim(-200, 200)
+
+    plt.gcf().gca().add_artist(b_circle)
 
     plt.show()
 
@@ -68,8 +99,8 @@ a = {
     "model": "NREL 5MW",
     "location":
     {
-      "x": 5,
-      "y": 0
+      "x": 20,
+      "y": 20
     }
   }
 
@@ -82,7 +113,7 @@ b = {
     }
   }
 
-wind = {"angle": 0}
+wind = {"angle": 15}
 
 
-inWake(a, b, 70, wind)
+inWake(a, b, 0, wind)
