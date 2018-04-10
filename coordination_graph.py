de0e105e3a2724dd49ebfd6f2ba8cd2b98abbe1c
Senne Deproost <sennedeproost@hotmail.com>
2018-04-10 17:56:23 +0200

    First prototype of the findOJA function

--- coordination_graph.py
+++ coordination_graph.py
@@ -13,10 +13,14 @@ def createCG(database, nActions, *parameters):
     entities = database.all()
     for entity in entities:
         g.printStat("   Adding node " + str(entity["id"]) + " to CG")
-        cg.add_node(entity["id"], data=entity, qFunction=np.zeros([nActions]))
+        cg.add_node(entity["id"],
+                    data=entity,
+                    qFunction=np.zeros([nActions, nActions]))
         for other in entities:
-            if dep.dependsOn(entity, other, parameters) and entity["id"] != other["id"]:
-                cg.add_edge(entity["id"], other["id"], valRules=np.zeros([nActions, nActions]))
+            if dep.dependsOn(entity, other, parameters)
+            and entity["id"] != other["id"]:
+                cg.add_edge(entity["id"], other["id"],
+                            valRules=np.zeros([nActions, nActions])) # Row = cur
     g.printStat("   Dependencies found: " + str(cg.edges()))
     return cg
 
@@ -36,7 +40,8 @@ def findInvolvement(agents, cg):
     return result
 
 
-def localQVal(agent, action, cg):
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def localQVal(agent, action,  cg):
     involvement = findInvolvement(agent , cg) # List of edges (a, b)
     valRules = []
     result = 0
@@ -49,57 +54,57 @@ def localQVal(agent, action, cg):
     return result / len(involvement)
 
 
+# Find the Optimal Joint Action (at the moment for 3 node involvment)
+def findOJA(cg):
+    graph = cg.copy()
+    counter = len(graph)
 
+    while counter > 1:
+        node = graph[counter]
+        neighbors = neighbors(counter)
+        neighborQFuncs = map(lambda x: graph[x]['qFunction'] , neighbors)
 
+        influenced = graph[graph.out_edges(counter)[0][1]]
+        influencer = graph[graph.in_edges(counter)[0][0]]
 
-        #if action in rule["actions"]:
-        #    result =+ rule["payoff"] / len(rule["agents"])
+        influencedActions = range(0, len(influenced['qFunction']))
+        influencerActions = range(0, len(influencer['qFunction']))
 
-    return result
+        nActions = len(influencedActions * influencerActions)
 
+        internalMaxFun = np.zeros([len(influencedActions), len(influencerActions)])
 
- #def updateValueRule(agent, action, valRule, valRules):
-    # agents = valRule["agents"]
-    # actions = valRule["actions"]
+        # For every action of the influenced en the influencer, the internalMaxFun
+        # returns the maximum of the sum of the Q values from the local Q function
+        # of the influenced variable and the Q function of the eliminated one.
+        for a1 in influencedActions:
+            for a2 in inflerAction:
+                result = max(np.add((influencedActions[a1],
+                                    (influencerActions[a2]))))
+                internalMaxFun[inflcedAction][inflencerAction] = result
 
+        # Eliminate the variable
+        graph.add_edge(
+        graph.in_edges(counter)[0][0],
+        graph.outedges(counter)[0][1]
+        )
 
+        graph.remove_edge(graph.in_edges[0])
+        graph.remove_edge(graph.out_edges[0])
 
+        graph.remove_node(counter)
 
+        counter -= 1
 
-    # reward =
+    # When all but one of the variables is eliminated, the optmal action of
+    # of the only variable left is calculated with max(internalMaxFun(action)).
+    # To calculate the optimal actions of the other variables, the algoritme
+    # traverses the graph backwards.
+    counter = len(graph)
+    optimalActions = []
 
+    for counter > 0:
+        optimalActions.append(graph[counter]['internalMaxFun'])
+        counter -= 1
 
-    # valRule.payoff = newPayoff
-
-
-
-
-    # valRules.write_back(updatedRule)
-
-
-
-
-
-
-
-
-
-#def findInvolvement(agent, valueRules)
-
-
-
-#def calculateLocalQ(agent, action, valueRules):
-#    involvement = findInvolvement(agent, action)
-
-
-
-
-
-#def updateValueRule(valueRule, reward):
-#    contect = valueRule["context"]
-#    payoff = valueRule["payoff"]
-#    return payoff + g.learningRate
-
-
-#def findOJA(CG, value_rules):
-#    return 5
+    return optimalActions
